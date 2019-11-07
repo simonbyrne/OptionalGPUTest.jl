4a529a3391cba6f28836c1618dac91f5b7b8aba2
Simon Byrne <simonbyrne@gmail.com>
2019-11-07 09:39:56 -0800

    update based on feedback from Tim Besard

--- src/OptionalGPUTest.jl
+++ src/OptionalGPUTest.jl
@@ -1,21 +1,15 @@
 module OptionalGPUTest
 
-using CUDAapi, GPUifyLoops
+using CuArrays, GPUifyLoops, CUDAnative
 
 export DefaultArray, mul2!, gpu_allowscalar
 
-if CUDAapi.has_cuda()
-    using CuArrays, CUDAnative
-    device(::CuArray) = CUDA()
-    gpu_allowscalar(x) = CuArrays.allowscalar(x)
-    const DefaultArray = CuArrays.CuArray
-else
-    gpu_allowscalar(x) = nothing
-    const DefaultArray = Array
-end
+__init__() = global DefaultArray = CuArrays.functional() ? CuArray : Array
+
+gpu_allowscalar(x) = CUDAapi.has_cuda() ? CuArrays.allowscalar(x) : nothing
 
 device(::AbstractArray) = CPU()
-
+device(::CuArray) = CUDA()
 
 function mul2_kernel!(A)
     @loop for i in (1:size(A,1);
